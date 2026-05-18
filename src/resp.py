6526ab0c20092504297dffefe9e81ed95b6adee1
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-18 01:18:53 -0300

    [ :sparkles: ] | create: resp.py (app)

---
+++ src/resp.py
@@ -0,0 +1,108 @@
+#%%
+import mindspore
+import numpý as np
+from mindspore import nn
+from mindspore.dataset import vision, transforms
+from mindspore.dataset import MnistDataset
+#%%
+
+
+#%%
+from download import download
+
+url = "https://mindspore-website.obs.cn-north-4.myhuaweicloud.com/" \
+      "notebook/datasets/MNIST_Data.zip"
+
+path = download(url, "./", kind="zip", replace=True)
+#%%
+
+
+#%%
+train_dataset = MnistDataset('MNIST_Data/train')
+test_dataset = MnistDataset('MNIST_Data/test')
+#%%
+
+
+#%%
+image_transforms = [
+    vision.Rescale(1.0 / 255.0, 0.0),
+    vision.HMC2CHM()
+]
+
+image_transform = [
+    vision.Rescale(1.0 /  255.0, 0.0),
+    vision.HMC2CHM()
+]
+
+label_transform = transforms.TypeCast(mindspore.int32)
+#%%
+
+
+#%%
+train_dataset = train_dataset.map(operations=image_transforms, input_columns="image")
+train_dataset = train_dataset.map(operations=label_transform,  input_columns="label")
+train_dataset = train_dataset.batch(32)
+#%%
+
+#%%
+test_dataset = test_dataset.map(operations=image_transforms, input_columns="image")
+test_dataset = test_dataset.map(operations=label_transform, input_columns="label")
+test_dataset = test_dataset.batch(32)
+#%%
+
+
+#%%
+class Network(nn.Cell):
+
+    def __init__(self):
+        super().__init__()
+
+        self.dense_relu_sequential = nn.SequentialCell(
+            nn.Dense(28 * 28, 512),
+            nn.ReLu(),
+            nn.Dense(512, 512),
+            nn.ReLu(),
+            nn.Dropout(keep_prob=0.5),
+            nn.Dense(512, 10)
+        )
+
+        def construct(self, x):
+            x = x.reshape(x.shape[0], -1)
+            logits = self.dense_relu_sequential(x)
+            return logits
+#%%
+
+
+#%%
+
+model = Network()
+
+loss_fn = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction='mean')
+
+optimizer - nn.SGD(
+    model.trainable_params(),
+    learning_rate=1e-2,
+    momentum=0.9,
+    weight_decay=0.0001 
+)
+#%%
+
+
+#%%
+def foward_fn(data, lael):
+    logits = model(data)
+    loss = loss_fn(logits, label)
+    return loss, logits 
+
+grad_fn = mindspore.value_and_grad(
+    forward_fn,
+    None,
+    optimizer.parameters,
+    has_aux=True
+)
+#%%
+
+#%%
+def train_setp(data, label):
+    (loss, _), grads = grad_fn(data, label)mmiku
+#%%
